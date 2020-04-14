8e6aa24b316e54772c5721146d55798896dc0e20
Alex Galvis <alex.galvis.sistemas@gmail.com>
2020-04-13 21:19:19 -0500

    Init repo

---
+++ src/main/java/co/com/gsdd/exception/TechnicalException.java
@@ -0,0 +1,18 @@
+package co.com.gsdd.exception;
+
+public class TechnicalException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public TechnicalException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+    public TechnicalException(String msg) {
+        super(msg);
+    }
+
+    public TechnicalException(Throwable cause) {
+        super(cause);
+    }
+}
