2b90078119f50416bbbc054d1ea3d5e5680f0fbd
Alex Galvis <alex.galvis.sistemas@gmail.com>
2021-12-14 13:11:38 -0500

    Update test class

--- src/test/java/com/gsdd/exception/TechnicalExceptionTest.java
+++ src/test/java/com/gsdd/exception/TechnicalExceptionTest.java
@@ -2,20 +2,19 @@ package com.gsdd.exception;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import com.gsdd.exception.TechnicalException;
 
 class TechnicalExceptionTest {
 
   private static final String ERROR = "Error";
 
   @Test
-  void getTechnicalExceptionWithMsgTest() {
+  void testGetTechnicalExceptionWithMsg() {
     TechnicalException et = new TechnicalException(ERROR);
     Assertions.assertEquals(ERROR, et.getMessage());
   }
 
   @Test
-  void getTechnicalExceptionWithCauseTest() {
+  void testGetTechnicalExceptionWithCause() {
     Throwable t = new Throwable(ERROR);
     String msj = "java.lang.Throwable: Error";
     TechnicalException et = new TechnicalException(t);
@@ -23,7 +22,7 @@ class TechnicalExceptionTest {
   }
 
   @Test
-  void getTechnicalExceptionWithMsgAndCauseTest() {
+  void testGetTechnicalExceptionWithMsgAndCause() {
     Throwable t = new Throwable(ERROR);
     TechnicalException et = new TechnicalException(ERROR, t);
     Assertions.assertEquals(ERROR, et.getMessage());
