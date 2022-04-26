4946dd72466083b646138507cd6b0db78c59e675
Alex Galvis <alex.galvis.sistemas@gmail.com>
2022-04-25 19:53:20 -0500

    Replace formatter plugin by spotless plugin

--- src/main/java/com/gsdd/exception/TechnicalException.java
+++ src/main/java/com/gsdd/exception/TechnicalException.java
@@ -2,17 +2,17 @@ package com.gsdd.exception;
 
 public class TechnicalException extends RuntimeException {
 
-  private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-  public TechnicalException(String msg, Throwable cause) {
-    super(msg, cause);
-  }
+    public TechnicalException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
 
-  public TechnicalException(String msg) {
-    super(msg);
-  }
+    public TechnicalException(String msg) {
+        super(msg);
+    }
 
-  public TechnicalException(Throwable cause) {
-    super(cause);
-  }
+    public TechnicalException(Throwable cause) {
+        super(cause);
+    }
 }
--- src/test/java/com/gsdd/exception/TechnicalExceptionTest.java
+++ src/test/java/com/gsdd/exception/TechnicalExceptionTest.java
@@ -5,27 +5,26 @@ import org.junit.jupiter.api.Test;
 
 class TechnicalExceptionTest {
 
-  private static final String ERROR = "Error";
+    private static final String ERROR = "Error";
 
-  @Test
-  void testGetTechnicalExceptionWithMsg() {
-    TechnicalException et = new TechnicalException(ERROR);
-    Assertions.assertEquals(ERROR, et.getMessage());
-  }
+    @Test
+    void testGetTechnicalExceptionWithMsg() {
+        TechnicalException et = new TechnicalException(ERROR);
+        Assertions.assertEquals(ERROR, et.getMessage());
+    }
 
-  @Test
-  void testGetTechnicalExceptionWithCause() {
-    Throwable t = new Throwable(ERROR);
-    String msj = "java.lang.Throwable: Error";
-    TechnicalException et = new TechnicalException(t);
-    Assertions.assertEquals(msj, et.getMessage());
-  }
+    @Test
+    void testGetTechnicalExceptionWithCause() {
+        Throwable t = new Throwable(ERROR);
+        String msj = "java.lang.Throwable: Error";
+        TechnicalException et = new TechnicalException(t);
+        Assertions.assertEquals(msj, et.getMessage());
+    }
 
-  @Test
-  void testGetTechnicalExceptionWithMsgAndCause() {
-    Throwable t = new Throwable(ERROR);
-    TechnicalException et = new TechnicalException(ERROR, t);
-    Assertions.assertEquals(ERROR, et.getMessage());
-  }
-
+    @Test
+    void testGetTechnicalExceptionWithMsgAndCause() {
+        Throwable t = new Throwable(ERROR);
+        TechnicalException et = new TechnicalException(ERROR, t);
+        Assertions.assertEquals(ERROR, et.getMessage());
+    }
 }
